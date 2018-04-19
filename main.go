48a2660c7dbc2c3747ff43264324b27006de1088
Stan Nelson <eleniums@yahoo.com>
2018-04-19 11:26:30 -0700

    Create main window and main loop

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/eleniums/game-of-life-go/assets"
-	_ "github.com/faiface/pixel"
+	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
-	_ "github.com/faiface/pixel/text"
 )
 
 func main() {
@@ -15,8 +17,47 @@ func main() {
 }
 
 func run() {
+	//interval := flag.Int("interval", 1000, "simulation update interval in ms")
+	resizable := flag.Bool("resizable", false, "allow resizing of the main window")
+	flag.Parse()
+
+	// load all assets
 	err := assets.Load()
 	if err != nil {
 		log.Fatalf("unable to load assets: %v", err)
+	}
+
+	// create new window
+	cfg := pixelgl.WindowConfig{
+		Title:     "Game of Life",
+		Bounds:    pixel.R(0, 0, 1260, 960),
+		VSync:     true, // update at the refresh rate of the monitor
+		Resizable: *resizable,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		log.Fatalf("unable to create window: %v", err)
+	}
+	win.SetSmooth(true) // remove pixelation
+
+	frames := 0
+	second := time.Tick(time.Second)
+
+	// main update loop
+	//last := time.Now()
+	for !win.Closed() {
+		//dt := time.Since(last).Seconds()
+		//last = time.Now()
+
+		win.Update()
+
+		// calculate FPS
+		frames++
+		select {
+		case <-second:
+			win.SetTitle(fmt.Sprintf("%s | FPS: %d", cfg.Title, frames))
+			frames = 0
+		default:
+		}
 	}
 }
