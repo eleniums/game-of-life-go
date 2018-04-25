b3e87824cbcbba0665cf31a036a581c62d42ebfd
Stan Nelson <eleniums@yahoo.com>
2018-04-24 21:37:22 -0700

    Add benchmarks for reproduction code

---
+++ game/reproduce_test.go
@@ -0,0 +1,37 @@
+package game
+
+import (
+	"testing"
+)
+
+func Benchmark_reproduceMajorityWins_WorstCase(b *testing.B) {
+	typeCounts := []int{1, 1, 1, 0}
+
+	for i := 0; i < b.N; i++ {
+		reproduceMajorityWins(typeCounts)
+	}
+}
+
+func Benchmark_reproduceMajorityWins_BestCase(b *testing.B) {
+	typeCounts := []int{3, 0, 0, 0}
+
+	for i := 0; i < b.N; i++ {
+		reproduceMajorityWins(typeCounts)
+	}
+}
+
+func Benchmark_reproduceRandomPercentage_WorstCase(b *testing.B) {
+	typeCounts := []int{1, 1, 1, 0}
+
+	for i := 0; i < b.N; i++ {
+		reproduceRandomPercentage(typeCounts)
+	}
+}
+
+func Benchmark_reproduceRandomPercentage_BestCase(b *testing.B) {
+	typeCounts := []int{3, 0, 0, 0}
+
+	for i := 0; i < b.N; i++ {
+		reproduceRandomPercentage(typeCounts)
+	}
+}
