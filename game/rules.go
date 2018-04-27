89262c5548ae86b4d16eebdd18c97a1c3e1f75ea
Stan Nelson <eleniums@yahoo.com>
2018-04-27 07:19:47 -0700

    Add rules to comment

--- game/rules.go
+++ game/rules.go
@@ -1,5 +1,10 @@
 package game
 
+// applyRules will determine if a cell should live or die, based on the following rules:
+// - If there are less than 2 living cells surrounding a living cell, it will die, as if by underpopulation.
+// - If there are 2 or 3 living cells surrounding a living cell, it will continue to live.
+// - If there are more than 3 living cells surrounding a living cell, it will die, as if by overpopulation.
+// - If there are exactly 3 living cells surrounding a dead cell, it will become alive, as if by reproduction.
 func applyRules(alive bool, neighbors int) bool {
 	if alive {
 		return neighbors >= 2 && neighbors <= 3
