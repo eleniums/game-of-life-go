2ccd38af602f4a4fa465a124769030c8812978e2
Stan Nelson <eleniums@yahoo.com>
2018-04-29 21:46:41 -0700

    Add CountNeighbors unit test

--- game/grid_test.go
+++ game/grid_test.go
@@ -2,7 +2,75 @@ package game
 
 import (
 	"testing"
+
+	assert "github.com/stretchr/testify/require"
 )
+
+func Test_CellGrid_CountNeighbors_Alive(t *testing.T) {
+
+	t.Run("Upper Left", func(t *testing.T) {
+		cells := NewCellGrid()
+		cells[4][6].Alive = true
+		count, _, _, _, _ := cells.CountNeighbors(5, 5)
+		assert.Equal(t, 1, count)
+	})
+
+	t.Run("Upper Middle", func(t *testing.T) {
+		cells := NewCellGrid()
+		cells[5][6].Alive = true
+		count, _, _, _, _ := cells.CountNeighbors(5, 5)
+		assert.Equal(t, 1, count)
+	})
+
+	t.Run("Upper Right", func(t *testing.T) {
+		cells := NewCellGrid()
+		cells[6][6].Alive = true
+		count, _, _, _, _ := cells.CountNeighbors(5, 5)
+		assert.Equal(t, 1, count)
+	})
+
+	t.Run("Middle Left", func(t *testing.T) {
+		cells := NewCellGrid()
+		cells[4][5].Alive = true
+		count, _, _, _, _ := cells.CountNeighbors(5, 5)
+		assert.Equal(t, 1, count)
+	})
+
+	t.Run("Center", func(t *testing.T) {
+		cells := NewCellGrid()
+		cells[5][5].Alive = true
+		count, _, _, _, _ := cells.CountNeighbors(5, 5)
+		assert.Equal(t, 0, count)
+	})
+
+	t.Run("Middle Right", func(t *testing.T) {
+		cells := NewCellGrid()
+		cells[6][5].Alive = true
+		count, _, _, _, _ := cells.CountNeighbors(5, 5)
+		assert.Equal(t, 1, count)
+	})
+
+	t.Run("Lower Left", func(t *testing.T) {
+		cells := NewCellGrid()
+		cells[4][4].Alive = true
+		count, _, _, _, _ := cells.CountNeighbors(5, 5)
+		assert.Equal(t, 1, count)
+	})
+
+	t.Run("Lower Middle", func(t *testing.T) {
+		cells := NewCellGrid()
+		cells[5][4].Alive = true
+		count, _, _, _, _ := cells.CountNeighbors(5, 5)
+		assert.Equal(t, 1, count)
+	})
+
+	t.Run("Lower Right", func(t *testing.T) {
+		cells := NewCellGrid()
+		cells[6][4].Alive = true
+		count, _, _, _, _ := cells.CountNeighbors(5, 5)
+		assert.Equal(t, 1, count)
+	})
+}
 
 func Benchmark_CellGrid_CountNeighbors_BestCase(b *testing.B) {
 	cells := NewCellGrid()
