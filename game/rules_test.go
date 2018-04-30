60501f77d02727e68b2eadfd3968db4af2aba7c5
Stan Nelson <eleniums@yahoo.com>
2018-04-30 06:53:17 -0700

    Add tests for rules

--- game/rules_test.go
+++ game/rules_test.go
@@ -2,7 +2,44 @@ package game
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func Test_applyRules_Underpopulation(t *testing.T) {
+	alive := applyRules(true, 1)
+	assert.False(t, alive)
+}
+
+func Test_applyRules_Overpopulation(t *testing.T) {
+	alive := applyRules(true, 4)
+	assert.False(t, alive)
+}
+
+func Test_applyRules_Reproduction(t *testing.T) {
+	alive := applyRules(false, 3)
+	assert.True(t, alive)
+}
+
+func Test_applyRules_StayAlive_2(t *testing.T) {
+	alive := applyRules(true, 2)
+	assert.True(t, alive)
+}
+
+func Test_applyRules_StayAlive_3(t *testing.T) {
+	alive := applyRules(true, 3)
+	assert.True(t, alive)
+}
+
+func Test_applyRules_StayDead_LessThan3(t *testing.T) {
+	alive := applyRules(false, 2)
+	assert.False(t, alive)
+}
+
+func Test_applyRules_StayDead_GreaterThan3(t *testing.T) {
+	alive := applyRules(false, 4)
+	assert.False(t, alive)
+}
 
 func Benchmark_applyRules_Underpopulation(b *testing.B) {
 	for i := 0; i < b.N; i++ {
