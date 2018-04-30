60961b3bc09eab329718b751290177b91fb5e633
Stan Nelson <eleniums@yahoo.com>
2018-04-30 07:28:11 -0700

    Simplify rule benchmarks

--- game/rules_test.go
+++ game/rules_test.go
@@ -30,44 +30,26 @@ func Test_applyRules(t *testing.T) {
 	}
 }
 
-func Benchmark_applyRules_Underpopulation(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		applyRules(true, 1)
+func Benchmark_applyRules(b *testing.B) {
+	benchmarks := []struct {
+		name      string
+		alive     bool
+		neighbors int
+	}{
+		{"Underpopulation", true, 1},
+		{"Overpopulation", true, 4},
+		{"Reproduction", false, 3},
+		{"Stay Alive With 2", true, 2},
+		{"Stay Alive With 3", true, 3},
+		{"Stay Dead With Less Than 3", false, 2},
+		{"Stay Dead With Greater Than 3", false, 4},
 	}
-}
 
-func Benchmark_applyRules_Overpopulation(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		applyRules(true, 4)
-	}
-}
-
-func Benchmark_applyRules_Reproduction(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		applyRules(false, 3)
-	}
-}
-
-func Benchmark_applyRules_StayAlive_2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		applyRules(true, 2)
-	}
-}
-
-func Benchmark_applyRules_StayAlive_3(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		applyRules(true, 3)
-	}
-}
-
-func Benchmark_applyRules_StayDead_LessThan3(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		applyRules(false, 2)
-	}
-}
-
-func Benchmark_applyRules_StayDead_GreaterThan3(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		applyRules(false, 4)
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				applyRules(bm.alive, bm.neighbors)
+			}
+		})
 	}
 }
