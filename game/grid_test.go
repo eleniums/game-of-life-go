833d107cf4328e2c4e702604e75a7ca0a8d967fd
Stan Nelson <eleniums@yahoo.com>
2018-05-01 19:30:44 -0700

    Update grid tests

--- game/grid_test.go
+++ game/grid_test.go
@@ -7,101 +7,210 @@ import (
 )
 
 func Test_CellGrid_CountNeighbors_Alive(t *testing.T) {
+	testCases := []struct {
+		name string
+		x    int
+		y    int
+		want int
+	}{
+		{"Upper Left", 4, 6, 3},
+		{"Upper Middle", 5, 6, 5},
+		{"Upper Right", 6, 6, 3},
+		{"Middle Left", 4, 5, 5},
+		{"Center", 5, 5, 8},
+		{"Middle Right", 6, 5, 5},
+		{"Lower Left", 4, 4, 3},
+		{"Lower Middle", 5, 4, 5},
+		{"Lower Right", 6, 4, 3},
+	}
 
-	t.Run("Upper Left", func(t *testing.T) {
+	cells := NewCellGrid()
+	for x := 4; x <= 6; x++ {
+		for y := 4; y <= 6; y++ {
+			cells[x][y].Alive = true
+		}
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			count, _, _, _, _ := cells.CountNeighbors(tc.x, tc.y)
+			assert.Equal(t, tc.want, count)
+		})
+	}
+}
+
+func Test_CellGrid_CountNeighbors_Type_Cross(t *testing.T) {
+	testCases := []struct {
+		name string
+		x    int
+		y    int
+		want int
+	}{
+		{"Upper Left", 4, 6, 3},
+		{"Upper Middle", 5, 6, 5},
+		{"Upper Right", 6, 6, 3},
+		{"Middle Left", 4, 5, 5},
+		{"Center", 5, 5, 8},
+		{"Middle Right", 6, 5, 5},
+		{"Lower Left", 4, 4, 3},
+		{"Lower Middle", 5, 4, 5},
+		{"Lower Right", 6, 4, 3},
+	}
+
+	cells := NewCellGrid()
+	for x := 4; x <= 6; x++ {
+		for y := 4; y <= 6; y++ {
+			cells[x][y].Alive = true
+			cells[x][y].Type = CellType_Cross
+		}
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, cross, _, _, _ := cells.CountNeighbors(tc.x, tc.y)
+			assert.Equal(t, tc.want, cross)
+		})
+	}
+}
+
+func Test_CellGrid_CountNeighbors_Type_Plus(t *testing.T) {
+	testCases := []struct {
+		name string
+		x    int
+		y    int
+		want int
+	}{
+		{"Upper Left", 4, 6, 3},
+		{"Upper Middle", 5, 6, 5},
+		{"Upper Right", 6, 6, 3},
+		{"Middle Left", 4, 5, 5},
+		{"Center", 5, 5, 8},
+		{"Middle Right", 6, 5, 5},
+		{"Lower Left", 4, 4, 3},
+		{"Lower Middle", 5, 4, 5},
+		{"Lower Right", 6, 4, 3},
+	}
+
+	cells := NewCellGrid()
+	for x := 4; x <= 6; x++ {
+		for y := 4; y <= 6; y++ {
+			cells[x][y].Alive = true
+			cells[x][y].Type = CellType_Plus
+		}
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, _, plus, _, _ := cells.CountNeighbors(tc.x, tc.y)
+			assert.Equal(t, tc.want, plus)
+		})
+	}
+}
+
+func Test_CellGrid_CountNeighbors_Type_Circle(t *testing.T) {
+	testCases := []struct {
+		name string
+		x    int
+		y    int
+		want int
+	}{
+		{"Upper Left", 4, 6, 3},
+		{"Upper Middle", 5, 6, 5},
+		{"Upper Right", 6, 6, 3},
+		{"Middle Left", 4, 5, 5},
+		{"Center", 5, 5, 8},
+		{"Middle Right", 6, 5, 5},
+		{"Lower Left", 4, 4, 3},
+		{"Lower Middle", 5, 4, 5},
+		{"Lower Right", 6, 4, 3},
+	}
+
+	cells := NewCellGrid()
+	for x := 4; x <= 6; x++ {
+		for y := 4; y <= 6; y++ {
+			cells[x][y].Alive = true
+			cells[x][y].Type = CellType_Circle
+		}
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, _, _, circle, _ := cells.CountNeighbors(tc.x, tc.y)
+			assert.Equal(t, tc.want, circle)
+		})
+	}
+}
+
+func Test_CellGrid_CountNeighbors_Type_Dot(t *testing.T) {
+	testCases := []struct {
+		name string
+		x    int
+		y    int
+		want int
+	}{
+		{"Upper Left", 4, 6, 3},
+		{"Upper Middle", 5, 6, 5},
+		{"Upper Right", 6, 6, 3},
+		{"Middle Left", 4, 5, 5},
+		{"Center", 5, 5, 8},
+		{"Middle Right", 6, 5, 5},
+		{"Lower Left", 4, 4, 3},
+		{"Lower Middle", 5, 4, 5},
+		{"Lower Right", 6, 4, 3},
+	}
+
+	cells := NewCellGrid()
+	for x := 4; x <= 6; x++ {
+		for y := 4; y <= 6; y++ {
+			cells[x][y].Alive = true
+			cells[x][y].Type = CellType_Dot
+		}
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, _, _, _, dot := cells.CountNeighbors(tc.x, tc.y)
+			assert.Equal(t, tc.want, dot)
+		})
+	}
+}
+
+func Benchmark_CellGrid_CountNeighbors(b *testing.B) {
+	b.Run("Best Case", func(b *testing.B) {
 		cells := NewCellGrid()
-		cells[4][6].Alive = true
-		count, _, _, _, _ := cells.CountNeighbors(5, 5)
-		assert.Equal(t, 1, count)
+
+		cells[4][5].Alive = false
+		cells[6][5].Alive = false
+		cells[5][6].Alive = false
+		cells[4][6].Alive = false
+		cells[6][6].Alive = false
+		cells[5][4].Alive = false
+		cells[4][4].Alive = false
+		cells[6][4].Alive = false
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			cells.CountNeighbors(5, 5)
+		}
 	})
 
-	t.Run("Upper Middle", func(t *testing.T) {
+	b.Run("Worst Case", func(b *testing.B) {
 		cells := NewCellGrid()
-		cells[5][6].Alive = true
-		count, _, _, _, _ := cells.CountNeighbors(5, 5)
-		assert.Equal(t, 1, count)
-	})
 
-	t.Run("Upper Right", func(t *testing.T) {
-		cells := NewCellGrid()
-		cells[6][6].Alive = true
-		count, _, _, _, _ := cells.CountNeighbors(5, 5)
-		assert.Equal(t, 1, count)
-	})
-
-	t.Run("Middle Left", func(t *testing.T) {
-		cells := NewCellGrid()
 		cells[4][5].Alive = true
-		count, _, _, _, _ := cells.CountNeighbors(5, 5)
-		assert.Equal(t, 1, count)
-	})
-
-	t.Run("Center", func(t *testing.T) {
-		cells := NewCellGrid()
-		cells[5][5].Alive = true
-		count, _, _, _, _ := cells.CountNeighbors(5, 5)
-		assert.Equal(t, 0, count)
-	})
-
-	t.Run("Middle Right", func(t *testing.T) {
-		cells := NewCellGrid()
 		cells[6][5].Alive = true
-		count, _, _, _, _ := cells.CountNeighbors(5, 5)
-		assert.Equal(t, 1, count)
-	})
-
-	t.Run("Lower Left", func(t *testing.T) {
-		cells := NewCellGrid()
-		cells[4][4].Alive = true
-		count, _, _, _, _ := cells.CountNeighbors(5, 5)
-		assert.Equal(t, 1, count)
-	})
-
-	t.Run("Lower Middle", func(t *testing.T) {
-		cells := NewCellGrid()
+		cells[5][6].Alive = true
+		cells[4][6].Alive = true
+		cells[6][6].Alive = true
 		cells[5][4].Alive = true
-		count, _, _, _, _ := cells.CountNeighbors(5, 5)
-		assert.Equal(t, 1, count)
-	})
-
-	t.Run("Lower Right", func(t *testing.T) {
-		cells := NewCellGrid()
+		cells[4][4].Alive = true
 		cells[6][4].Alive = true
-		count, _, _, _, _ := cells.CountNeighbors(5, 5)
-		assert.Equal(t, 1, count)
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			cells.CountNeighbors(5, 5)
+		}
 	})
-}
-
-func Benchmark_CellGrid_CountNeighbors_BestCase(b *testing.B) {
-	cells := NewCellGrid()
-
-	cells[4][5].Alive = false
-	cells[6][5].Alive = false
-	cells[5][6].Alive = false
-	cells[4][6].Alive = false
-	cells[6][6].Alive = false
-	cells[5][4].Alive = false
-	cells[4][4].Alive = false
-	cells[6][4].Alive = false
-
-	for i := 0; i < b.N; i++ {
-		cells.CountNeighbors(5, 5)
-	}
-}
-
-func Benchmark_CellGrid_CountNeighbors_WorstCase(b *testing.B) {
-	cells := NewCellGrid()
-
-	cells[4][5].Alive = true
-	cells[6][5].Alive = true
-	cells[5][6].Alive = true
-	cells[4][6].Alive = true
-	cells[6][6].Alive = true
-	cells[5][4].Alive = true
-	cells[4][4].Alive = true
-	cells[6][4].Alive = true
-
-	for i := 0; i < b.N; i++ {
-		cells.CountNeighbors(5, 5)
-	}
 }
