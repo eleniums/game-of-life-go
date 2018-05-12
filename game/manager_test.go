ca9af0021b2e53897023ccf1cb34554bbd36c30c
Stan Nelson <eleniums@yahoo.com>
2018-05-12 11:55:35 -0700

    Add some manager tests

--- game/manager_test.go
+++ game/manager_test.go
@@ -2,7 +2,41 @@ package game
 
 import (
 	"testing"
+
+	assert "github.com/stretchr/testify/require"
 )
+
+func Test_Manager_swapBuffer(t *testing.T) {
+	manager := NewManager()
+
+	manager.cells[25][25].Alive = true
+	manager.buffer[5][5].Alive = true
+
+	manager.swapBuffer()
+
+	assert.False(t, manager.cells[25][25].Alive)
+	assert.True(t, manager.cells[5][5].Alive)
+
+	assert.True(t, manager.buffer[25][25].Alive)
+	assert.False(t, manager.buffer[5][5].Alive)
+}
+
+func Test_Manager_updateBuffer(t *testing.T) {
+	manager := NewManager()
+	cells := manager.Cells()
+
+	cells[24][25].Alive = true
+	cells[25][25].Alive = true
+	cells[26][25].Alive = true
+
+	manager.updateBuffer()
+
+	assert.False(t, manager.buffer[24][25].Alive)
+	assert.True(t, manager.buffer[25][25].Alive)
+	assert.False(t, manager.buffer[26][25].Alive)
+	assert.True(t, manager.buffer[25][24].Alive)
+	assert.True(t, manager.buffer[25][26].Alive)
+}
 
 func Benchmark_Manager_swapBuffer(b *testing.B) {
 	manager := NewManager()
